1144b4be6a6cd8ebdec61f129c8ba79bc83a427c
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-03-12 20:32:12 +0900

    buttonの変更

--- app/components/molecules/LineUp.tsx
+++ app/components/molecules/LineUp.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "../atoms/button";
 import { ArrowRight } from "lucide-react";
 
 const LineUp = () => {
