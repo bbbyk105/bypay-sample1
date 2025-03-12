9db08a4affb5d51281640188d292b877b341aad7
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-03-12 19:48:09 +0900

    Productsの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import LineUp from "./components/molecules/LineUp";
-import News from "./components/molecules/News";
+import Products from "./components/molecules/Products";
 import Hero from "./components/organisms/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <News />
       <LineUp />
+      <Products />
     </>
   );
 }
