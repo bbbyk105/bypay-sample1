01645fb30c18d2d9ff7c7aefbee26e8bf0840072
Byakko Kondo <bbbyk105@yahoo.co.jp>
2025-03-24 14:39:14 +0900

    heroの編集

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,39 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
+import { Button } from "../components/atoms/Button";
 
-const page = () => {
-  return <div>page</div>;
+const EmptyCartPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+      {/* カートの空メッセージ */}
+      <div className="text-center space-y-6">
+        <h3 className="text-2xl font-semibold text-gray-800">カートが空です</h3>
+
+        <Link href="/">
+          <Button
+            className={cn(
+              "bg-gray-300 hover:bg-amber-600 shadow-lg transition-all duration-300"
+            )}
+            size="lg"
+            variant="default"
+          >
+            買い物を続ける
+          </Button>
+        </Link>
+      </div>
+
+      {/* 後で購入 */}
+      <div className="mt-10 w-full max-w-lg bg-white shadow-lg rounded-xl p-6 border">
+        <h4 className="text-lg font-semibold text-gray-900">後で購入</h4>
+        <p className="text-gray-600 mt-2 text-sm leading-relaxed">
+          保存されたアイテムはまだありません。
+          <br />
+          このリストには、後で購入するアイテムを追加できます。
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default EmptyCartPage;
